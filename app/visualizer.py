3620ecd5f67bc605086c859259aa2bdb3d6f8b07
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2020-09-25 15:21:52 +0530

    Visualizer fixed for Heroku
    * Introduced logger in visualizer.py
    
    * Force mkdir to graph generate directory

--- app/visualizer.py
+++ app/visualizer.py
@@ -6,43 +6,51 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import os
 import secrets
+import logging
 
 def visualize(handle):
-    url = 'https://codeforces.com/contests/with/'
-    url += handle
-    html_page = requests.get(url)
-    soup = BeautifulSoup(html_page.content, 'html5lib')
-    table = soup.find('table', attrs = {'class' : 'user-contests-table'})
-    ratings_tr = table.findAll('tr')
-    ratings_tr = ratings_tr[1:]
-    ratings_list = []
-    for this_row in ratings_tr:
-        ratings_list.append(int(this_row.findAll('td')[5].text))
-    ratings_list.reverse()
-    ratings_df = pd.DataFrame(ratings_list, columns = ['RATINGS'])
-    ratings_df.head()
-    idx_df = pd.DataFrame(np.arange(1, len(ratings_list) + 1, dtype='int64'), columns = ['CONTEST'])
-    idx_df.head()
-    df = pd.concat([idx_df, ratings_df], axis = 1)
-    df.columns = ['CONTEST', 'RATINGS']
-    df.head()
-    model = LinearRegression()
-    model.fit(idx_df, ratings_df)
-    
+    logger = logging.Logger(__name__)
+    try:
+        url = 'https://codeforces.com/contests/with/'
+        url += handle
+        html_page = requests.get(url)
+        soup = BeautifulSoup(html_page.content, 'html5lib')
+        logger.info("Successfully parsed soup")
+        table = soup.find('table', attrs = {'class' : 'user-contests-table'})
+        ratings_tr = table.findAll('tr')
+        ratings_tr = ratings_tr[1:]
+        ratings_list = []
+        for this_row in ratings_tr:
+            ratings_list.append(int(this_row.findAll('td')[5].text))
+        ratings_list.reverse()
+        ratings_df = pd.DataFrame(ratings_list, columns = ['RATINGS'])
+        ratings_df.head()
+        idx_df = pd.DataFrame(np.arange(1, len(ratings_list) + 1, dtype='int64'), columns = ['CONTEST'])
+        idx_df.head()
+        df = pd.concat([idx_df, ratings_df], axis = 1)
+        df.columns = ['CONTEST', 'RATINGS']
+        df.head()
+        model = LinearRegression()
+        model.fit(idx_df, ratings_df)
 
-    # Visualization
+        # Visualization
 
-    plt.plot(idx_df, ratings_df)
-    plt.title('Username : {}'.format(handle))
-    plt.xlabel('Contest ID')
-    plt.ylabel('Ratings')
-    dummy_dataset = pd.DataFrame(np.arange(0, ratings_df.shape[0]))
-    plt.plot(dummy_dataset, model.predict(dummy_dataset))
-    output_filename = secrets.token_hex(16) + '.png'
-    path = os.path.abspath('.') + '/app/static/generated/'
-    plt.savefig(path + output_filename)
-    plt.close()
-    return output_filename
+        plt.plot(idx_df, ratings_df)
+        plt.title('Username : {}'.format(handle))
+        plt.xlabel('Contest ID')
+        plt.ylabel('Ratings')
+        dummy_dataset = pd.DataFrame(np.arange(0, ratings_df.shape[0]))
+        plt.plot(dummy_dataset, model.predict(dummy_dataset))
+        output_filename = secrets.token_hex(16) + '.png'
+        path = os.path.abspath('.') + '/app/static/generated/'
+        os.makedirs(path, exist_ok = True)
+        plt.savefig(path + output_filename)
+        plt.close()
+        return output_filename
+    except Exception as e:
+        msg = 'Couldn\'t generate graph'
+        logger.warning(msg)
+        raise Exception(e)
 
 
 
